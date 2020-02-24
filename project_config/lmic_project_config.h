09b9cf260726ba7734ce9ea3747dc12b165be1b7
alec <jagnese2018@fau.edu>
2020-02-24 08:31:03 -0500

    first commit

---
+++ project_config/lmic_project_config.h
@@ -0,0 +1,18 @@
+// project-specific definitions
+//#define CFG_eu868 1
+#define CFG_us915 1
+//#define CFG_au915 1
+//#define CFG_as923 1
+// #define LMIC_COUNTRY_CODE LMIC_COUNTRY_CODE_JP	/* for as923-JP */
+//#define CFG_kr920 1
+//#define CFG_in866 1
+#define CFG_sx1276_radio 1
+#define LMIC_USE_INTERRUPTS
+#define DISABLE_PING
+#define DISABLE_BEACONS
+#define DISABLE_MCMD_RXParamSetupReq
+#define DISABLE_MCMD_DlChannelReq
+#define DISABLE_MCMD_RXTimingSetupReq
+#define DISABLE_FSK
+#define LMIC_ENABLE_long_messages 1    
+#define DISABLE_HALCONFIG
